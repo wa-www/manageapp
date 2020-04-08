983d6984797c140b36815323072c0f3817098e84
Wa-www <0nicoco0@gmail.com>
2020-04-08 22:58:37 +0900

    devise/omiauth
    導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+<html>
+  <head>
+    studyマネ
+  </head>
+  <body>
+    <p class="notice"><%= notice %></p>
+    <p class="alert"><%= alert %></p>
+  </body>
+</html>
+
+
 end

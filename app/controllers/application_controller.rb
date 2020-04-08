bd9e4bd03cc0c7aeb143da81b044ddf41d0df7d0
Wa-www <0nicoco0@gmail.com>
2020-04-08 23:25:25 +0900

    modify
    modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-
-<html>
-  <head>
-    studyマネ
-  </head>
-  <body>
-    <p class="notice"><%= notice %></p>
-    <p class="alert"><%= alert %></p>
-  </body>
-</html>
-
-
 end
